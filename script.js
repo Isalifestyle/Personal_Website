24f385801ccdacee1dec4ba315e90c966a7104f9
Isalifestyle <oisa@uncc.edu>
2024-10-25 18:38:19 -0400

    Finished the nav bar, introduction, and 'About Me' sections.

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Buttons
+let button = document.querySelector('.project-button');
+let element = document.querySelector('.colmar'); // Select the element to show/hide
+
+function toggleDropdown() {
+    if (element.style.display === 'none' || element.style.display === '') {
+        element.style.display = 'grid';
+    } else {
+        element.style.display = 'none';
+    }
+}
+
+// Add an event listener to the button for toggling
+button.addEventListener('click', toggleDropdown);
